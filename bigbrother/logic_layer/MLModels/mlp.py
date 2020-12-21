cfbc3252ead3f9535266b182248f828b2a4ae5c6
Sergio <sagg_301@outlook.com>
2020-12-21 11:55:32 -0400

    oops

--- bigbrother/logic_layer/MLModels/mlp.py
+++ bigbrother/logic_layer/MLModels/mlp.py
@@ -1,20 +1,16 @@
 # mlp for regression
 import numpy as np
 import pandas as pd
-from pandas.core.arrays import categorical
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
-from keras.models import Sequential
-from keras.layers import Dense
-from sklearn.metrics import confusion_matrix
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, regularizers
+from tensorflow.keras.layers.experimental import preprocessing
 from django_pandas.io import read_frame
 from ...models import CrimesData
-from sklearn.compose import ColumnTransformer 
-import tensorflow as tf
 import datetime as dt
 
-# import sys
-# np.set_printoptions(threshold=sys.maxsize)
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 # load the dataset
 
 class MLPModel(object):
@@ -25,7 +21,7 @@ class MLPModel(object):
         """
         Load the data
         """
-        crimes = CrimesData.objects.all()[:10000]
+        crimes = CrimesData.objects.all()
         df = read_frame(crimes)
 
         return df
@@ -38,64 +34,64 @@ class MLPModel(object):
         # Part 1 - Data Preprocessing
         # Importing the dataset
         dataset = self.load_data()
+        dataset = dataset.drop(labels=['case_number','block', 'iucr', 'id'], axis=1)\
 
         #Transform date to integer
         dataset['date'] = pd.to_datetime(dataset['date'])
-        dataset['date']=dataset['date'].map(dt.datetime.toordinal)  
+        dataset['date']=dataset['date'].map(dt.datetime.toordinal)
 
-        X = dataset[['date','primary_type']].values
-        y = dataset['district'].values
+        dataset = pd.get_dummies(dataset,columns=['primary_type', 'district'])
 
-        # Encoding categorical data
-        labelencoder_X = LabelEncoder()
-        X[:,1] = labelencoder_X.fit_transform(X[:,1])
+        dataset = dataset.dropna()
 
-        ct = ColumnTransformer([("onehot", OneHotEncoder(handle_unknown='ignore'), [1])], remainder="passthrough") # The last arg ([0]) is the list of columns you want to transform in this step
-        X = ct.fit_transform(X).toarray()
+        train_dataset = dataset.sample(frac=0.66, random_state= 1)
+        test_dataset = dataset.drop(train_dataset.index)
 
-        # Splitting the dataset into the Training set and Test set
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+        #Variables independientes o features
+        train_features = train_dataset.copy()
+        test_features = test_dataset.copy()
 
-        # Feature Scaling
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
+        #Variables dependientes o labels
+        train_labels = train_features.pop('date')
+        test_labels = test_features.pop('date')
 
-        # Part 2 - Now let's make the ANN!
+        FEATURES = 60
+        N_VALIDATION = int(1e3)
+        N_TRAIN = int(1e4)
+        BUFFER_SIZE = int(1e4)
+        BATCH_SIZE = 500
+        STEPS_PER_EPOCH = N_TRAIN//BATCH_SIZE
 
-        # Importing the Keras libraries and packages
+        #Normalizar
+        normalizer = preprocessing.Normalization()
+        normalizer.adapt(np.array(train_features))
 
-        # Initialising the ANN
-        model = Sequential()
+        model = keras.Sequential([
+            normalizer,
+            layers.Dense(512,activation='relu', input_shape=(FEATURES,)),
+            layers.Dense(512,activation='relu'),
+            layers.Dense(512,activation='relu'),
+            layers.Dense(512,activation='relu'),
+            layers.Dense(1)
+        ])
+        lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+            0.001,
+            decay_steps=STEPS_PER_EPOCH*1000,
+            decay_rate=1,
+            staircase=False)
 
-        # Adding the input layer and the first hidden layer
-        model.add(Dense(units = 64, kernel_initializer = 'uniform', activation = 'relu', input_dim = 27))
+        model.compile(loss='mean_absolute_error',
+                        optimizer=tf.keras.optimizers.Adam(lr_schedule))
+        
+        print(model.summary())
+        history = model.fit(
+        train_features, train_labels,
+        steps_per_epoch = STEPS_PER_EPOCH,
+        validation_split=0.3,
+        verbose=1, epochs=1000)
 
-        # Adding the second hidden layer
-        model.add(Dense(units = 64, kernel_initializer = 'uniform', activation = 'relu'))
-
-        # Adding the third hidden layer
-        model.add(Dense(units = 64, kernel_initializer = 'uniform', activation = 'relu'))
-
-        # Adding the output layer
-        model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-
-        model.summary()
-
-        # Compiling the ANN
-        model.compile(optimizer = tf.optimizers.Adam(learning_rate=0.1), loss = 'mean_absolute_error', metrics = ['accuracy'])
-
-        # Fitting the ANN to the Training set
-        model.fit(X_train, y_train, batch_size = 10, epochs = 100, verbose= 0)
-
-        model.save('trained-model')
-
-        # Part 3 - Making the predictions and evaluating the model
-
-        # Predicting the Test set results
-        y_pred = model.predict(X_test)
-        y_pred = (y_pred > 0.5)
-        print(y_pred)
+        test_results = model.evaluate(test_features, test_labels, verbose=0)
+        print(test_results)
 
         # Making the Confusion Matrix
         # cm = confusion_matrix(y_test, y_pred)
